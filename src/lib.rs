0303b56bca940e8ed4698e291fd2009ea790ba08
Wei Tang <hi@that.world>
2017-11-07 20:08:00 +0800

    Add updating hmac logic

--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,12 @@
 #![no_std]
 
-#[macro_use]
-extern crate typenum;
+extern crate hmac;
 extern crate digest;
 extern crate generic_array;
 
 use digest::{Input, BlockInput, FixedOutput};
 use generic_array::{ArrayLength, GenericArray};
-use typenum::Quot;
-use typenum::consts::*;
-use core::ops::Div;
+use hmac::{Mac, Hmac, MacResult};
 
 pub struct HmacDrbg<D>
     where D: Input + BlockInput + FixedOutput + Default,
@@ -17,8 +14,8 @@ pub struct HmacDrbg<D>
           D::OutputSize: ArrayLength<u8>
 {
     digest: D,
-    k: GenericArray<u8, D::OutputSize>,
-    v: GenericArray<u8, D::OutputSize>,
+    k: MacResult<D::OutputSize>,
+    v: MacResult<D::OutputSize>,
 }
 
 impl<D> HmacDrbg<D>
@@ -26,19 +23,56 @@ impl<D> HmacDrbg<D>
           D::BlockSize: ArrayLength<u8>,
           D::OutputSize: ArrayLength<u8>
 {
-    pub fn raw(
-        digest: D,
-        k: GenericArray<u8, D::OutputSize>,
-        v: GenericArray<u8, D::OutputSize>
-    ) -> Self {
-        Self { digest, k, v }
+    pub fn new(entropy: &[u8], nonce: &[u8], pers: &[u8]) -> Self {
+        let mut k = GenericArray::<u8, D::OutputSize>::default();
+        let mut v = GenericArray::<u8, D::OutputSize>::default();
+
+        for i in 0..k.as_slice().len() {
+            k[i] = 0x0;
+        }
+
+        for i in 0..v.as_slice().len() {
+            v[i] = 0x01;
+        }
+
+        let mut this = Self {
+            digest: D::default(),
+            k: MacResult::new(k),
+            v: MacResult::new(v),
+        };
+
+        this.update(&[entropy, nonce, pers]);
+
+        this
     }
 
-    pub fn k(&self) -> &GenericArray<u8, D::OutputSize> {
-        &self.k
+    fn hmac(&self) -> Hmac<D> {
+        Hmac::new(self.k.code())
     }
 
-    pub fn v(&self) -> &GenericArray<u8, D::OutputSize> {
-        &self.v
+    fn update(&mut self, seeds: &[&[u8]]) {
+        let mut kmac = self.hmac();
+        kmac.input(self.v.code());
+        kmac.input(&[0x00]);
+        for seed in seeds {
+            kmac.input(seed);
+        }
+        self.k = kmac.result();
+
+        let mut vmac = self.hmac();
+        vmac.input(self.v.code());
+        self.v = vmac.result();
+
+        let mut kmac = self.hmac();
+        kmac.input(self.v.code());
+        kmac.input(&[0x01]);
+        for seed in seeds {
+            kmac.input(seed);
+        }
+        self.k = kmac.result();
+
+        let mut vmac = self.hmac();
+        vmac.input(self.v.code());
+        self.v = vmac.result();
     }
 }
--- tests/lib.rs
+++ tests/lib.rs
@@ -1,7 +1,5 @@
 #![no_std]
 
-#[macro_use]
-extern crate typenum;
 extern crate hmac_drbg;
 extern crate sha2;
 extern crate generic_array;
@@ -9,10 +7,3 @@ extern crate generic_array;
 use hmac_drbg::*;
 use sha2::Sha256;
 use generic_array::GenericArray;
-
-#[test]
-fn generic_array_size() {
-    let drbg = HmacDrbg::<Sha256>::raw(Sha256::default(), GenericArray::default(), GenericArray::default());
-    assert_eq!(drbg.k().as_slice().len(), 256 / 8);
-    assert_eq!(drbg.v().as_slice().len(), 256 / 8);
-}
